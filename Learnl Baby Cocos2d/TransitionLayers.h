cce754b196c8bbbd5e85c2ab24d9e9303d24c379
Fred Sharples <fred@orangedesign.com>
2013-04-02 22:15:47 -0700

    Initial setup of the 2 transition layers

---
+++ Learnl Baby Cocos2d/TransitionLayers.h
@@ -0,0 +1,19 @@
+//
+//  TransitionLayers.h
+//  Learnl Baby Cocos2d
+//
+//  Created by Fred Sharples on 4/2/13.
+//  Copyright (c) 2013 Fred Sharples. All rights reserved.
+//
+
+#import "CCLayer.h"
+#import "cocos2d.h"
+#define TRANSITION_DURATION (1.2f)
+
+
+@interface TransitionLayer_A : CCLayer
+
+@end
+@interface TransitionLayer_B : CCLayer
+
+@end
---
+++ Learnl Baby Cocos2d/TransitionLayers.m
@@ -0,0 +1,17 @@
+//
+//  TransitionLayers.m
+//  Learnl Baby Cocos2d
+//
+//  Created by Fred Sharples on 4/2/13.
+//  Copyright (c) 2013 Fred Sharples. All rights reserved.
+//
+//Sets up 2 layers to transition to and from.
+#import "TransitionLayers.h"
+
+@implementation TransitionLayer_A
+
+@end
+
+@implementation TransitionLayer_B
+
+@end
