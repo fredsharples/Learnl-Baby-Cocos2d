bba2459b504c16b5f5a6ce41ef9c8165c16a16ca
Fred Sharples <fred@orangedesign.com>
2013-04-29 16:51:01 -0700

    Fixed problems with resources not being added to target

--- Learnl Baby Cocos2d/LegalLayer.m
+++ Learnl Baby Cocos2d/LegalLayer.m
@@ -45,6 +45,7 @@
 		
 		if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) {
 			background = [CCSprite spriteWithFile:@"screen_legal.png"];
+            //background = [CCSprite spriteWithFile:@"Default.png"];
 			background.rotation = 90;
 		} else {
 			background = [CCSprite spriteWithFile:@"Default-Landscape~ipad.png"];
@@ -61,7 +62,7 @@
 -(void) onEnter
 {
 	[super onEnter];
-    [self performSelector:@selector(nextScene) withObject:nil afterDelay:0.5f];
+    [self performSelector:@selector(nextScene) withObject:nil afterDelay:1.5f];
 }
 -(void) nextScene
 {
